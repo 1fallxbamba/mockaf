841e711f706b6c4792d186bd0d22099fe9f18c01
1fallxbamba <fbamba99@gmail.com>
2022-04-12 15:57:55 +0000

    update app_keys

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     url: env('MY_HEROKU_URL'),
     proxy: true,
     app: {
-      keys: env.array("APP_KEYS"),
+      keys: env.array("APP_KEYS", ["ktJ8QqsXSnlCfWT4SPkgPA==", "8mDaF7C9FZwQlrR0JkFh7w=="]),
     },
   });
   
